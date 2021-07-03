b61f6e2daa04c4fb2a45b45b54e3793fab2af1b2
Caleb Odinga <calemasanga@gmail.com>
2021-07-03 13:29:44 +0300

    adds image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+  image_url = models.ImageField(upload_to = 'gallery/')
+  image_name = models.CharField(max_length=60)
+  image_description = models.TextField()
+
+  def __str__(self):
+      return self.image_name
+  
--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'personal_gallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.personal_gallery',
+        'NAME':'personal_gallery',
         'USER':'moringa',
         'PASSWORD':'Access'
     }
