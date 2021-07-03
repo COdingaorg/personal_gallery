9005f4b4a4d80490b0365228cd7a8a1628120acf
Caleb Odinga <calemasanga@gmail.com>
2021-07-03 18:05:08 +0300

    rectifies error in settings

--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'personal_gallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join (BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.templates.context_processors.media',
+                'django.template.context_processors.media',
             ],
         },
     },
