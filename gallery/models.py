fedd1d836f0746976294a9d1ceb30e8d2fa603d4
Caleb Odinga <calemasanga@gmail.com>
2021-07-03 13:58:59 +0300

    adds location and category models, and date posted column in image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Image(models.Model):
   image_url = models.ImageField(upload_to = 'gallery/')
   image_name = models.CharField(max_length=60)
   image_description = models.TextField()
+  date_taken = models.DateField(default=dt.date.today())
 
   def __str__(self):
       return self.image_name
+
+class Location(models.Model):
+  location_name = models.CharField(max_length=100)
+
+  def __str__(self):
+      return self.location_name
+
+class Categories(models.Model):
+  category_name = models.CharField(max_length= 100)
+  
+
+
+
   
