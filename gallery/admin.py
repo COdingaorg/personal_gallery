e4b26c6e5fd8db9e2546c7fff277a5156dff56f2
Caleb Odinga <calemasanga@gmail.com>
2021-07-03 14:37:41 +0300

    adds foreign key in imgae model

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from gallery.models import Image, Location, Categories
 
+class ImageAdmin(admin.ModelAdmin):
+  filter_horizontal=('category')
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Categories)
--- gallery/models.py
+++ gallery/models.py
@@ -2,15 +2,6 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
-class Image(models.Model):
-  image_url = models.ImageField(upload_to = 'gallery/')
-  image_name = models.CharField(max_length=60)
-  image_description = models.TextField()
-  date_taken = models.DateField(default=dt.date.today())
-
-  def __str__(self):
-      return self.image_name
-
 class Location(models.Model):
   location_name = models.CharField(max_length=100)
 
@@ -19,7 +10,23 @@ class Location(models.Model):
 
 class Categories(models.Model):
   category_name = models.CharField(max_length= 100)
-  
+
+  def __str__(self):
+    return self.category_name
+
+class Image(models.Model):
+  image_url = models.ImageField(upload_to = 'gallery/')
+  image_name = models.CharField(max_length=60)
+  image_description = models.TextField()
+  date_taken = models.DateField(default=dt.date.today())
+  category = models.ForeignKey(Categories, on_delete=models.CASCADE, default=1)
+  location_taken = models.ForeignKey(Location, on_delete=models.CASCADE, default=1)
+
+  def __str__(self):
+      return self.image_name
+
+
+
 
 
 
