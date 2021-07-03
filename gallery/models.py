ba86506ec0db81cdf96c8649f30b913901b0b9a5
Caleb Odinga <calemasanga@gmail.com>
2021-07-03 17:15:39 +0300

    adds test for savins and deleting class category

--- gallery/models.py
+++ gallery/models.py
@@ -10,12 +10,21 @@ class Location(models.Model):
 
   def save_location(self):
     self.save()
+  
+  def delete_location(self):
+    self.delete()
 
 class Categories(models.Model):
   category_name = models.CharField(max_length= 100)
 
   def __str__(self):
     return self.category_name
+
+  def save_category(self):
+    self.save()
+  
+  def delete_category(self):
+    self.delete()
 
 class Image(models.Model):
   image_url = models.ImageField(upload_to = 'gallery/')
@@ -30,12 +39,4 @@ class Image(models.Model):
 
   def save_image(self):
     self.save()
-  
-
-
-
-
-
-
-
   
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,4 +1,4 @@
-from gallery.models import Image, Location
+from gallery.models import Categories, Image, Location
 from django.db.models.deletion import SET_NULL
 from django.db.models.fields import TextField
 from django.test import TestCase
@@ -18,3 +18,48 @@ class TestLocation(TestCase):
     self.new_location.save_location()
     locations = Location.objects.all()
     self.assertTrue(len(locations)>0)
+  
+  #test update method
+  # def test_update_location(self):
+  #   self.new_location.save()
+  #   Location.objects.filter(pk = 1).update(location_name = 'Manyatta')
+    
+  #   self.new_location.refresh_from_db()
+
+  #   self.assertTrue(self.new_location == 'Manyatta')
+
+  def test_delete_location(self):
+    self.new_location.save_location()
+    self.new_location.delete_location()
+    locations = Location.objects.all()
+    self.assertTrue(len(locations)==0)
+
+class TestCategories(TestCase):
+  #setup method
+  def setUp(self):
+    self.new_category = Categories(category_name = 'E-bikes')
+
+  #test instance
+  def test_instance_creation(self):
+    self.assertTrue(isinstance(self.new_category, Categories))
+  
+  #test save method
+  def test_save_category(self):
+    self.new_category.save_category()
+    categories = Categories.objects.all()
+    self.assertTrue(len(categories)>0)
+  
+  #test update method
+  # def test_update_category(self):
+  #   self.new_category.save()
+  #   Categories.objects.filter(category_name = 'E-bike').update(category_name = 'Nature')
+  #   self.new_category.refresh_from_db()
+  #   self.assertTrue(self.new_category == 'Nature')
+
+  def test_delete_category(self):
+    self.new_category.save_category()
+    self.new_category.delete_category()
+    categories = Categories.objects.all()
+    self.assertTrue(len(categories)==0)
+
+
