4524609b7af93b8ee3e096e84a70d234643cc81b
Caleb Odinga <calemasanga@gmail.com>
2021-07-03 15:14:37 +0300

    adds instance and save tests on class location and passes them

--- gallery/models.py
+++ gallery/models.py
@@ -6,7 +6,10 @@ class Location(models.Model):
   location_name = models.CharField(max_length=100)
 
   def __str__(self):
-      return self.location_name
+    return self.location_name
+
+  def save_location(self):
+    self.save()
 
 class Categories(models.Model):
   category_name = models.CharField(max_length= 100)
@@ -19,11 +22,14 @@ class Image(models.Model):
   image_name = models.CharField(max_length=60)
   image_description = models.TextField()
   date_taken = models.DateField(default=dt.date.today(), blank=True)
-  category = models.ForeignKey(Categories, on_delete=models.CASCADE, default=1)
-  location_taken = models.ForeignKey(Location, on_delete=models.CASCADE, default=1)
+  category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+  location_taken = models.ForeignKey(Location, on_delete=models.CASCADE)
 
   def __str__(self):
       return self.image_name
+
+  def save_image(self):
+    self.save()
   
 
 
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,14 +1,20 @@
-from gallery.models import Image
+from gallery.models import Image, Location
 from django.db.models.deletion import SET_NULL
 from django.db.models.fields import TextField
 from django.test import TestCase
 
 # Create your tests here.
-class TestImageClass(TestCase):
-  #set up method
+class TestLocation(TestCase):
+  #setup method
   def setUp(self):
-      self.new_image = Image(image_url='/gallery/static/images/bmw.jpeg', image_name='BMW coupe', image_description='Red bmw coupe')
+    self.new_location = Location(location_name = 'Kibra')
 
   #test instance
-  def test_instance(self):
-    self.assertTrue(isinstance(self.new_image, Image))
+  def test_instance_creation(self):
+    self.assertTrue(isinstance(self.new_location, Location))
+  
+  #test save method
+  def test_save_location(self):
+    self.new_location.save_location()
+    locations = Location.objects.all()
+    self.assertTrue(len(locations)>0)
