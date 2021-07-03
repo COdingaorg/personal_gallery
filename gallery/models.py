c08c98a066f6a2c08ecfb08f8abf2a25abcdb11a
Caleb Odinga <calemasanga@gmail.com>
2021-07-03 14:50:58 +0300

    adds blank true on image model date column

--- gallery/models.py
+++ gallery/models.py
@@ -18,12 +18,14 @@ class Image(models.Model):
   image_url = models.ImageField(upload_to = 'gallery/')
   image_name = models.CharField(max_length=60)
   image_description = models.TextField()
-  date_taken = models.DateField(default=dt.date.today())
+  date_taken = models.DateField(default=dt.date.today(), blank=True)
   category = models.ForeignKey(Categories, on_delete=models.CASCADE, default=1)
   location_taken = models.ForeignKey(Location, on_delete=models.CASCADE, default=1)
 
   def __str__(self):
       return self.image_name
+  
+
 
 
 
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,14 @@
+from gallery.models import Image
+from django.db.models.deletion import SET_NULL
+from django.db.models.fields import TextField
 from django.test import TestCase
 
 # Create your tests here.
+class TestImageClass(TestCase):
+  #set up method
+  def setUp(self):
+      self.new_image = Image(image_url='/gallery/static/images/bmw.jpeg', image_name='BMW coupe', image_description='Red bmw coupe')
+
+  #test instance
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_image, Image))
