8768a0ab3ffa94b94f7f564599fecf6810ce72de
Caleb Odinga <calemasanga@gmail.com>
2021-07-03 23:35:55 +0300

    adds images display to homepage

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from .models import Image, Categories, Location
 
 # Create your views here.
 def index(request):
   title = 'sue gallery'
+  images = Image.objects.all()[3:9]
+  allimages = Image.objects.all()
 
-  return render(request, 'welcome.html', {'title':title})
+  image1 = Image.objects.get(pk = 1)
+  image2 = Image.objects.get(pk = 2)
+  image3 = Image.objects.get(pk = 3)
+
+  return render(request, 'welcome.html', {'title':title, 'images':images, 'allimages':allimages,
+  'image1':image1, 'image2':image2, 'image3':image3})
 
 def gallery_disp(request):
   pass
