3cd9e5dbc7c647e8df80f0c60b070c222cb1d2d6
Caleb Odinga <calemasanga@gmail.com>
2021-07-04 20:39:20 +0300

    links single urls to images

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^$', views.index, name = 'home'), 
-  url(r'^image/(d+)/', views.single_image, name = 'single_image'),
+  url(r'^$', views.index, name = 'home'),
   url(r'^gallery_disp/$', views.gallery_disp, name='gallery_disp'),
   url(r'^search/', views.search_images, name='search_images'),
- 
+  url(r'^image/(\d+)/$', views.single_image, name = 'single_image'),
 ]
