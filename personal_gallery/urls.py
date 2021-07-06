29cd771711183df8a34608917e7065ea74ea5be9
Caleb Odinga <calemasanga@gmail.com>
2021-07-06 21:09:11 +0300

    adds base html template for registration

--- personal_gallery/urls.py
+++ personal_gallery/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'',views.index, name = 'home'),
-    path(r'^gallery/', include('gallery.urls'))
+    path(r'^gallery/', include('gallery.urls')),
+    path(r'^accounts/', include('registration.backends.simple.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
