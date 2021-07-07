3b69cbd6b341a8e7694b82c9501832bff5d480d6
Caleb Odinga <calemasanga@gmail.com>
2021-07-07 20:43:48 +0300

    refines profile link in navbar, adds profile template

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
   url(r'^$', views.index, name = 'home'),
@@ -8,5 +7,4 @@ urlpatterns = [
   url(r'^search/', views.search_images, name='search_images'),
   url(r'^image/(\d+)/$', views.single_image, name = 'single_image'),
   url(r'^images_today/$', views.images_today, name = 'images_today'),
-  url(r'^profile/$', views.profile, name='profile')
 ]
